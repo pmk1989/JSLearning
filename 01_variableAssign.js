f47d3004278136a6b11ab1c971e49280d9476013
pmk1989 <140337082+pmk1989@users.noreply.github.com>
2023-09-20 15:34:34 +0530

    designed Variable assignment JS
    designed Variable assignment JS

---
+++ 01_variableAssign.js
@@ -0,0 +1,33 @@
+console.log("**********PART-01**************");
+
+var myFullName; // variable declaration
+console.log(myFullName);
+myFullName = "PRAMOD KUMBHAR";  // Variable initialization
+console.log("FULL NAME :",myFullName);
+myFullName = "PRAMOD MALLIKARJUN KUMBHAR"; // Variable update
+console.log("FULL NAME :",myFullName);
+
+console.log("**********PART-02**************");
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+console.log("*********PART-03***************")
+
+var firstName = "PRAMOD";
+console.log(firstName);
+var lastName = "KUMBHAR";
+console.log(lastName);
+var age = "33 years";
+console.log(age);
+var address = "New Panvel , Raigad";
+console.log(address);
+var pinCode = "410206";
+console.log(pinCode);
+var gender = "Male";
+console.log(gender);
+var maritalStatus = "Unmarried";
+console.log(maritalStatus);
+
+console.log("*******THE END***********");
+

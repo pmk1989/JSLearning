7dfabaa4bd9ec8d60af3f7f17a4339c9233e98ef
pmk1989 <140337082+pmk1989@users.noreply.github.com>
2023-09-21 17:02:06 +0530

    dataType Assignment of JS
    dataType Assignment of JS

---
+++ 02_typeOf.js
@@ -0,0 +1,46 @@
+
+console.log("********************PART - 01***********************");
+
+console.log("----------Before Initialization------------");
+
+var bankName;
+console.log("BANK NAME : ",bankName,"| DATA TYPE : ",bankName);
+
+console.log("----------After Initialization------------");
+
+var bankName = "SBI Bank";
+console.log("BANK NAME : ",bankName,"| DATA TYPE : ",bankName);
+
+console.log("********************PART - 02***********************");
+
+var marks = "90%";
+console.log("Marks : ",marks,"| DATA TYPE : ", typeof marks);
+
+console.log("********************PART - 03***********************");
+
+console.log("----------Before Initialization------------");
+
+var isWorking;
+console.log("Are you working : ",isWorking,"| DATA TYPE : ",typeof isWorking);
+
+console.log("----------After Initialization------------");
+
+var isWorking = true;
+console.log("Are you working : ",isWorking,"| DATA TYPE : ",typeof isWorking);
+
+console.log("********************PART - 04***********************");
+
+console.log("---------- Current ------------");
+
+var totalCount = "Hundred and Seven";
+console.log("Total Count : ",totalCount,"| DATA TYPE : ",typeof totalCount);
+
+console.log("---------- Updated ------------");
+
+var totalCount = 107;
+console.log("Total Count : ",totalCount,"| DATA TYPE : ",typeof totalCount);
+
+console.log("---------- END ------------");
+
+
+
---
+++ 02_variableBasicsAssign.js
@@ -0,0 +1,44 @@
+
+console.log("************PART - 01****************");
+
+console.log("--------NUMBERS--------");
+
+var rice = 56.23;
+console.log("Rate of 1kg Rice =>",rice, "Data Type =>",typeof rice);
+
+var wheat = 12.3;
+console.log("Rate of 1kg Wheat =>",wheat,"Data Type =>", typeof wheat);
+
+var bajara = 34;
+console.log("Rate of 1kg Bajara =>",bajara,"Data Type =>", typeof bajara);
+
+console.log("--------STRING---------");
+
+var firstName = "PRAMOD";
+console.log("FIRST NAME : ",firstName,"Data Type =>",typeof firstName);
+
+var middleName = "MALLIKARJUN";
+console.log("MIDDLE NAME : ",middleName,"Data Type =>",typeof middleName);
+
+var lastName = "KUMBHAR";
+console.log("LAST NAME : ",lastName,"Data Type =>",typeof lastName);
+
+console.log("---------Boolean----------");
+
+var isEmployed = true;
+console.log("Are you Employed : ",isEmployed,"Data Type =>", typeof isEmployed);
+
+var isIndian = true;
+console.log("Are you Indian : ",isIndian,"Data Type =>", typeof isIndian);
+
+var isUnMarried = true;
+console.log("Are you UnMarried : ",isUnMarried,"Data Type =>", typeof isUnMarried);
+
+console.log("*******************PART - 02***********************");
+
+console.log('--------null vs undefined-------');
+
+var undefiend;
+console.log("Undefined variable means which is not Initialized : ",undefiend," Data Type =>",typeof undefiend);
+undefiend = null
+console.log("Null value means that ther is no value it is empty :",null," Data Type =>",null);

edcd7f76e79c7c0c314086393d07773c0e1d0114
pmk1989 <140337082+pmk1989@users.noreply.github.com>
2023-09-25 16:01:04 +0530

    String and Function Assignment in JS
    String and Function Assignment in JS

---
+++ 03_funAssign02.js
@@ -0,0 +1,8 @@
+
+
+function bankDetails(bankName,accountNum,location,pinCode){
+    console.log("BANK NAME : ",bankName,"ACCOUNT NUMBER : ",accountNum,"| LOCATION : ",location,"| PINCODE : ",pinCode);
+    }
+    bankDetails('CITI BANK |',3456782345,'PUNE',431202);
+    bankDetails('AXIS BANK |',7856782345,'MUMBAI',441202);
+    bankDetails('HDFC BANK |',8956782345,'PUNE',631202,"OPEN");
---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,32 @@
+console.log("**************PART - 01****************");
+
+function stringBasics(){
+console.log(" No argument and No return ");
+}
+stringBasics();
+
+console.log("**************PART - 02****************");
+
+function dreamCompany(companyName){
+    console.log(" My dream company is : ",companyName);
+    }
+    dreamCompany("TATA Communication Services (TCS)");
+   
+    console.log("**************PART - 03****************");
+
+    
+    var hobby1 = "Handball";
+    var hobby2 = " Astrology";
+    var hobby3 = " Reading";
+
+   console.log("========== concatenation =============== ");
+
+    var result = hobby1 + hobby2 + hobby3 ;
+    console.log(result);   
+
+    console.log("========== String Length =============== ");
+
+var length = result.length;
+console.log("Total numbers of characters that is available in hobby1,hobby2,hobby3 :",result.length);
+
+console.log("========== END =============== ");

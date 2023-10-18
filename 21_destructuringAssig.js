1c8e4107105b1b3c21d18215ea7dee886da6c67b
pmk1989 <140337082+pmk1989@users.noreply.github.com>
2023-10-18 17:05:27 +0530

    destructuring assignment in JS
    destructuring assignment in JS

---
+++ 21_destructuringAssig.js
@@ -0,0 +1,27 @@
+
+console.log('=============Task 1=====================');
+
+const arrayNum = [11,3,4,11,4,7,3];
+
+const uniqueNumber = [...new Set(arrayNum)];
+
+console.log(`Given array is : [11,3,4,11,4,7,3]`);
+
+console.log("After removing duplicate element : ",uniqueNumber);
+
+console.log('=============Task 2=====================');
+
+function alternateText(str){
+
+    var chars = str.toLowerCase().split("");
+
+    for (let index = 0; index < chars.length; index = index+2) {
+        chars[index] = chars[index].toUpperCase();
+        
+    }
+    return chars.join('');
+}
+console.log(`Given string value is : How are you mate`);
+console.log("Output of the given string is : ",alternateText(`How are you mate`));
+
+console.log("============== THE END ======================");

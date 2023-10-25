8f3ce7c28a23958ca3fde1c2784acc1944407be4
pmk1989 <140337082+pmk1989@users.noreply.github.com>
2023-10-25 14:03:34 +0530

    DOM assignment in js
    DOM assignment in js

--- 27_reduce.js
+++ 27_reduce.js
@@ -22,3 +22,28 @@ const arraySum = arrayNum.reduce((runningTotal1,value1)=>{
     return runningTotal1 + value1;
 }, 0);
 console.log(arraySum);
+
+
+const str = "How are you mate"; // "HoW ArE YoU MatE"
+
+const arrayWord = str.split(' ');
+
+let finalStr = '';
+
+for (const word of arrayWord) {
+
+    let firstChar = word.charAt(0).toUpperCase();
+
+    let lastChar = word.charAt(word.length-1).toUpperCase();
+
+    // Replace word,  first char by firstChar
+
+    // Replace word,  last char by lastChar
+
+    finalStr = finalStr + word;
+
+    console.log(word, firstChar, lastChar );
+
+}
+
+console.log(finalStr);
